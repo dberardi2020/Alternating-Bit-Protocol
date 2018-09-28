e338208a0d459d413c91f207e6f46db5ecc80a37
Dimitri Berardi <Dimitri.Berardi@gmail.com>
2018-09-27 20:54:45 -0400

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+# the compiler: gcc for C program, define as g++ for C++
+CC = gcc
+
+# compiler flags:
+#  -g    adds debugging information to the executable file
+#  -Wall turns on most, but not all, compiler warnings
+CFLAGS  = -g
+
+OBJS = project2.o student2.o
+
+# the build target executable:
+TARGET = project2
+
+all: $(TARGET)
+
+project2: $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o project2
+
+project2.o: project2.c
+	$(CC) $(CFLAGS) -c project2.c student2.c
+
+clean:
+	rm -f *~ *.o project2
---
+++ student2.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "project2.h"
+
+/* ***************************************************************************
+ ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
+
+ This code should be used for unidirectional or bidirectional
+ data transfer protocols from A to B and B to A.
+ Network properties:
+ - one way network delay averages five time units (longer if there
+ are other messages in the channel for GBN), but can be larger
+ - packets can be corrupted (either the header or the data portion)
+ or lost, according to user-defined probabilities
+ - packets may be delivered out of order.
+
+ Compile as gcc -g project2.c student2.c -o p2
+ **********************************************************************/
+
+/********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
+/* 
+ * The routines you will write are detailed below. As noted above, 
+ * such procedures in real-life would be part of the operating system, 
+ * and would be called by other procedures in the operating system.  
+ * All these routines are in layer 4.
+ */
+
+int sequence, count = 0;
+struct msg buffer;
+
+int checkSum(char data[20]) {
+	int checkSum = 0;
+	for (int i = 0; i < 20; i++) {
+		checkSum += data[i];
+	}
+	return checkSum;
+}
+/* 
+ * A_output(message), where message is a structure of type msg, containing 
+ * data to be sent to the B-side. This routine will be called whenever the 
+ * upper layer at the sending side (A) has a message to send. It is the job 
+ * of your protocol to insure that the data in such a message is delivered 
+ * in-order, and correctly, to the receiving side upper layer.
+ */
+void A_output(struct msg message) {
+
+	struct pkt newPkt;
+
+	newPkt.seqnum = sequence % 2;
+
+	/* create packets from incoming messages */
+	for (int i = 0; i < sizeof(message.data); i++) {
+		newPkt.payload[i] = message.data[i];
+		buffer.data[i] = message.data[i];
+	}
+
+	/* Run all messages through checkSum method */
+	/* Send to Layer 3, i.e. The Network Layer */
+	newPkt.checksum = checkSum(newPkt.payload);
+
+	printf("A_output: Sending Message: %s\n", newPkt.payload);
+
+	startTimer(0, 20.0);
+	tolayer3(0, newPkt);
+}
+
+/*
+ * Just like A_output, but residing on the B side.  USED only when the 
+ * implementation is bi-directional.
+ */
+void B_output(struct msg message) {
+	printf("B_output: Only used when implementation is bi-directional\n");
+}
+
+/* 
+ * A_input(packet), where packet is a structure of type pkt. This routine 
+ * will be called whenever a packet sent from the B-side (i.e., as a result 
+ * of a tolayer3() being done by a B-side procedure) arrives at the A-side. 
+ * packet is the (possibly corrupted) packet sent from the B-side.
+ */
+void A_input(struct pkt packet) {
+	// struct msg newMsg;
+	printf("A_input: reading ACK from B\n");
+	stopTimer(0);
+
+	if (packet.acknum == 0 && sequence % 2 == 0) {
+		sequence++;
+	} else if (packet.acknum == 1 && sequence % 2 == 1) {
+		sequence++;
+	} else {
+		A_output(buffer);
+	}
+}
+
+/*
+ * A_timerinterrupt()  This routine will be called when A's timer expires 
+ * (thus generating a timer interrupt). You'll probably want to use this 
+ * routine to control the retransmission of packets. See starttimer() 
+ * and stoptimer() in the writeup for how the timer is started and stopped.
+ */
+void A_timerinterrupt() {
+	A_output(buffer);
+}
+
+/* The following routine will be called once (only) before any other    */
+/* entity A routines are called. You can use it to do any initialization */
+void A_init() {
+}
+
+/* 
+ * Note that with simplex transfer from A-to-B, there is no routine  B_output() 
+ */
+
+/*
+ * B_input(packet),where packet is a structure of type pkt. This routine 
+ * will be called whenever a packet sent from the A-side (i.e., as a result 
+ * of a tolayer3() being done by a A-side procedure) arrives at the B-side. 
+ * packet is the (possibly corrupted) packet sent from the A-side.
+ */
+void B_input(struct pkt packet) {
+	int checkSumB;
+	struct msg newMsg;
+	struct pkt ackPkt;
+
+	/* calculate checksum and send to layer 3 */
+	checkSumB = checkSum(packet.payload);
+	if (checkSumB != packet.checksum) {
+		ackPkt.acknum = -1;
+		tolayer3(1, ackPkt);
+	} else { /*  */
+		for (int i = 0; i < 20; i++) {
+			newMsg.data[i] = packet.payload[i];
+		}
+
+		printf("B_input: Message Received: %s\n", newMsg.data);
+		tolayer5(1, newMsg);
+		printf("B_input: Sending to Layer 3\n");
+		ackPkt.acknum = packet.seqnum;
+		tolayer3(1, ackPkt);
+	}
+}
+
+/*
+ * B_timerinterrupt()  This routine will be called when B's timer expires 
+ * (thus generating a timer interrupt). You'll probably want to use this 
+ * routine to control the retransmission of packets. See starttimer() 
+ * and stoptimer() in the writeup for how the timer is started and stopped.
+ */
+void B_timerinterrupt() {
+}
+
+/* 
+ * The following routine will be called once (only) before any other   
+ * entity B routines are called. You can use it to do any initialization 
+ */
+void B_init() {
+}
+
